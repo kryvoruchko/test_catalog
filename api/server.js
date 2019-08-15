e74ae826e036d6fa37fe17261f809cb7e79c709f
Sergey Kryvoruchko <sinclar96@gmail.com>
2019-08-15 23:11:11 +0300

    delete swagger

--- api/server.js
+++ api/server.js
@@ -5,14 +5,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(jwt());
 
